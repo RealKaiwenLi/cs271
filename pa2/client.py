3cc53cbecefc710c67d9ce273ca61260256b9d98
RealKaiwenLi <72473351+RealKaiwenLi@users.noreply.github.com>
2022-01-26 12:45:01 -0800

    object

---
+++ pa2/client.py
@@ -0,0 +1,8 @@
+class client(object):
+    def __init__(self, host, port, incoming, outgoing):
+        self.host = host
+        self.port = port
+        self.incoming = incoming
+        self.outgoing = outgoing
+
+
